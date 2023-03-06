33ec500ba26e386701c88192139ee651ed71752d
Brandon Matran <b_matran@comcast.net>
2023-03-05 21:54:10 -0800

    MP3 player finished

--- nightowl/src/components/MusicControls.js
+++ nightowl/src/components/MusicControls.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import { ReactComponent as Play} from '../assets/play.png'
+import React from "react";
+import { ReactComponent as Play } from "../assets/play.svg";
+import { ReactComponent as Prev } from "../assets/prev.svg";
+import { ReactComponent as Pause } from "../assets/pause.svg";
+import { ReactComponent as Next } from "../assets/next.svg";
+import './MusicControl.css'
 
 const MusicControls = ({
   isPlaying,
@@ -8,20 +12,35 @@ const MusicControls = ({
   onNextClick,
 }) => (
   <div className="audio-controls">
-    {/* <button
+    <button
       type="button"
       className="prev"
-      aria-label="Previous"
       onClick={onPrevClick}
+      aria-label="Prev"
     >
       <Prev />
-    </button> */}
-    <button
-    type="button"
-    className="play"
-    onClick={() => onPlayPauseClick(true)}
-    aria-label="Play">
+    </button>
+    {isPlaying ? (
+      <button
+        type="button"
+        className="pause"
+        onClick={() => onPlayPauseClick(false)}
+        aria-label="Pause"
+      >
+        <Pause />
+      </button>
+    ) : (
+      <button
+        type="button"
+        className="play"
+        onClick={() => onPlayPauseClick(true)}
+        aria-label="Play"
+      >
         <Play />
+      </button>
+    )}
+    <button type="button" className="next" onClick={onNextClick}>
+      <Next />
     </button>
   </div>
 );
--- nightowl/src/track.js
+++ nightowl/src/track.js
@@ -1,13 +1,22 @@
 import kintsugi from "./assets/Kintsugi.wav";
+import gold from "./assets/Gold.mp3"
 import hollow from "./assets/hollow.jpg";
+import saidthesky from "./assets/saidthesky.jpeg"
 const tracks = [
   {
-    title: "Hollow",
+    title: "Kintsugi",
     artist: "Dabin",
     audioSrc: kintsugi,
     image: hollow,
     color: "#00aeb0",
   },
+  {
+    title: "Gold",
+    artist: "Said The Sky",
+    audioSrc: gold,
+    image: saidthesky,
+    color: "#00aeb0",
+  }
 ];
 
 export default tracks
