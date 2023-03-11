571fa3a704db73810759f7cb052a9ca4061de633
Brandon Matran <b_matran@comcast.net>
2023-03-11 14:38:42 -0800

    Front end started for shop

--- nightowl/src/components/Shop.js
+++ nightowl/src/components/Shop.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 export const Shop = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    async function getItems() {
+      let url = `http://localhost:5000/shop`;
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        setItems(data);
+      }
+    }
+    getItems();
+  }, []);
+
   return (
-    <div>Shop</div>
-  )
-}
+    <div className="container">
+      {items.map((item, index) => {
+        return (
+          <ul key={index}>
+            <li>{item.name}</li>
+            <li>{item.itemType}</li>
+            <li>{item.color}</li>
+            <li>{item.price}</li>
+          </ul>
+        );
+      })}
+    </div>
+  );
+};
